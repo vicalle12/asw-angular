e45e39d92c5b39377e5915977ed02acf1db88e9e
victor <vicalle49@gmail.com>
2016-04-23 10:48:38 +0000

    bonito casi

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,20 @@
 class StaticPagesController < ApplicationController
+  helper_method :getAuthor
+  
+  
   def home
-    @contributions = Contribution.all.order(created_at: :desc)
+    if params[:submission_type] == "show"
+      @contributions = Contribution.where.not(url: '').order(created_at: :desc)
+    elsif params[:submission_type] == "ask"
+      @contributions = Contribution.where.not(content: '').order(created_at: :desc)
+    else
+      @contributions = Contribution.all.order(created_at: :desc)
+    end
+  end
+  
+  def getAuthor(user_id)
+    @user = User.find(user_id)
+    author = @user.name
+    return author    
   end
 end

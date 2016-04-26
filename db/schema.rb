f2a0b23dfb03300acc5cdfd5ec0ad7fad41a9ed6
GerardDuch <gerard.duch@est.fib.upc.edu>
2016-04-26 08:23:00 +0000

    merge

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20160423111626) do
-#ActiveRecord::Schema.define(version: 20160423084545) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "content"

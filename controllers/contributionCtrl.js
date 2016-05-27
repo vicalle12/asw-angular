323c9ac4a94428196fba1b67787ce3c1f4fca5ec
Victor del Valle <vicalle49@gmail.com>
2016-05-28 00:35:14 +0200

    show contribution

--- controllers/contributionCtrl.js
+++ controllers/contributionCtrl.js
@@ -1,5 +1,13 @@
-angular.module('routing').controller('ContributionController', ContributionController);
+angular.module('routing').controller('ContributionController', ['$scope', '$http', '$stateParams', function($scope, $http, $stateParams) {
+    $http.get("https://hackernewsasw2016.herokuapp.com/contributions/"+$stateParams.contributionId+".json")
+    .then(function(response) {
+      $scope.titulo = response.data.titulo;
+      $scope.puntos = response.data.puntos;
+      $http.get("https://hackernewsasw2016.herokuapp.com/users/"+response.data.user_id+".json").then(function(response2) {
+        $scope.autor = response2.data.name;  
+      });
+      $scope.url = response.data.url;
+      $scope.date = response.data.created_at;
+    });
+}]);
 
-function ContributionController($scope, $rootScope) {
-    console.log("gasdfs");
-}

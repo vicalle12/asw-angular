6a7da34af5d88a555060e77612ce6b74354db2d8
Felix Hortelano <felix.hortelano@est.fib.upc.edu>
2016-05-29 13:36:19 +0200

    contributions con voto harcoded

--- controllers/homeCtrl.js
+++ controllers/homeCtrl.js
@@ -1,18 +1,16 @@
-angular.module('routing').controller('HomeController', ['$scope', '$http',
+angular.module('routing').controller('homeController', ['$scope', '$http',
     function ($scope, $http) {
 
         var SERVER_URL = 'https://hackernewsasw2016.herokuapp.com/contributions.json';
 
-        var contribuciones = [];
 
         $http.get(SERVER_URL).then(function (response) {
             $scope.contributions = response.data;
             console.log("HOLA");
-            /*
             for (var i=0; i<$scope.contributions.length; ++i) {
-                getAutores(i, $scope.contributions[i].id);
+                var autores = getAutores(i);
+                var comments = getComments (i, $scope.contributions)
             }
-            */
             /*
             $http.get("https://hackernewsasw2016.herokuapp.com/users/"+response.data.user_id+".json").then(function(response2) {
                 $scope.contributions.autor = response2.data.name;
@@ -20,21 +18,42 @@ angular.module('routing').controller('HomeController', ['$scope', '$http',
             */
         });
 
-        /*
-        var getAutores = function (i, j) {
+
+        var getAutores = function (i) {
             $http.get("https://hackernewsasw2016.herokuapp.com/users/"+$scope.contributions[i].user_id+".json").then(function(response2) {
-                $scope.autor = response2.data.name;
-                contribuciones.push(j + "/" + $scope.autor);
-                //console.log(contribuciones);
-                console.log(contribuciones);
+                $scope.contributions[i].autor = response2.data.name;
             });
         }
-        */
 
-        $scope.getAuthor = function (i) {
-            $http.get("https://hackernewsasw2016.herokuapp.com/users/"+i+".json").then(function(response2) {
-                return response2.data.name;
-            });
+        var getComments = function (i, object) {
+            $http.get("https://hackernewsasw2016.herokuapp.com/comments.json?contribution_id="+object[i].id+"")
+                .then(function(responsecomments) {
+                    $scope.contributions[i].comments = responsecomments.data.length;
+                    $scope.contributions[i].replies = 0;
+                    $scope.contrComments = responsecomments.data;
+                    //console.log($scope.contrComments);
+                    for (var j = 0; j < $scope.contrComments.length; j++) {
+                        var replies = getReplies(i, j, $scope.contrComments);
+
+                    }
+
+                });
+        }
+
+        var getReplies = function(i, j, object){
+            $http.get("https://hackernewsasw2016.herokuapp.com/replies.json?comment_id="+object[j].id)
+                .success(function(data) {
+                    $scope.contributions[i].replies += data.length;
+                    console.log(data);
+                });
+        }
+
+        $scope.voteContribution = function(id){
+            $http.put("https://hackernewsasw2016.herokuapp.com/contributions/"+id+"/like", { user_token: "MQ"} )
+                .success(function(data, status) {
+                    console.log("he votado");
+                    window.location.reload();
+                });
         }
 
 

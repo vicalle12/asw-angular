a52f4ab40b669f65c66eb4c7e896fbf56db50410
Oriol Momino <oriol.momino@est.fib.upc.edu>
2016-05-30 23:12:04 +0200

    success submit

--- controllers/submitCtrl.js
+++ controllers/submitCtrl.js
@@ -5,13 +5,8 @@ angular.module('routing').controller('submitController', ['$scope', '$http',
       $http.post("https://hackernewsasw2016.herokuapp.com/contributions/", { user_token: localStorage.getItem('hackerNewsToken'),titulo: $scope.contributionTitle,text: $scope.contributionContent,url: $scope.contributionUrl } )
       .success(function(data, status) {
         console.log(data);
-		if (status != 400) {
-			window.location.href="/";
-			window.location.href;
-		}
-		else { 
-			window.location.reload();
-		}
+		window.location.href="/";
+		window.location.href;
       });
     }  
 

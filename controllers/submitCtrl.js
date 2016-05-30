6b19d1002c9bcf969914880a634034c242d80945
Oriol Momino <oriol.momino@est.fib.upc.edu>
2016-05-30 23:10:16 +0200

    submit works falta redireccionar

--- controllers/submitCtrl.js
+++ controllers/submitCtrl.js
@@ -1,11 +1,17 @@
 angular.module('routing').controller('submitController', ['$scope', '$http',
     function ($scope, $http) {
 	
-	$scope.sendComment = function(){
+	$scope.submitContribution = function(){
       $http.post("https://hackernewsasw2016.herokuapp.com/contributions/", { user_token: localStorage.getItem('hackerNewsToken'),titulo: $scope.contributionTitle,text: $scope.contributionContent,url: $scope.contributionUrl } )
       .success(function(data, status) {
         console.log(data);
-        window.location.href="#/";
+		if (status != 400) {
+			window.location.href="/";
+			window.location.href;
+		}
+		else { 
+			window.location.reload();
+		}
       });
     }  
 

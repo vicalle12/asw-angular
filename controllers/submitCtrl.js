9215b6e2718d4f13730c81c83e8ff8945445849a
Oriol Momino <oriol.momino@est.fib.upc.edu>
2016-05-30 23:33:22 +0200

    redirect works

--- controllers/submitCtrl.js
+++ controllers/submitCtrl.js
@@ -5,8 +5,8 @@ angular.module('routing').controller('submitController', ['$scope', '$http',
       $http.post("https://hackernewsasw2016.herokuapp.com/contributions/", { user_token: localStorage.getItem('hackerNewsToken'),titulo: $scope.contributionTitle,text: $scope.contributionContent,url: $scope.contributionUrl } )
       .success(function(data, status) {
         console.log(data);
-		var landingUrl = window.location.host;
-		window.location.href = landingUrl;
+		//var landingUrl = window.location.host;
+		window.location.href = "/";
       })
 	  .error(function(data, status) {
 		  window.location.reload();
